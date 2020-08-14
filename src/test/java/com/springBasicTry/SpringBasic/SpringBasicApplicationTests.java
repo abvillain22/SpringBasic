bb3f26706451574155028ee1dc79bceab970320f
dheerajcse0101@gmail.com <dheerajsingh1234>
2020-08-14 22:36:21 +0530

    first Commit

---
+++ src/test/java/com/springBasicTry/SpringBasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBasicTry.SpringBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
